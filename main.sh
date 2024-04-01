177ddd00601dc207587d46fa14cc59ac3bda06e6
spring-reader <spring_reader@hotmail.com>
2024-04-01 07:45:10 +0000

    main: Optimize the self-update process
    Signed-off-by: spring-reader <spring_reader@hotmail.com>

--- main.sh
+++ main.sh
@@ -2,42 +2,62 @@
 
 WORK_DIR=`cd $(dirname $0); pwd;`
 
-TIMEOUT_DAYS=$((1 * 24 * 60 * 60))  # 1days
+CHECK_UPDATE_DAYS=$((1 * 24 * 60 * 60))  # 1days
 MAIN_SELF=$$
 CLIENT_CONF="${WORK_DIR}/c_client.conf"
 
 source ${WORK_DIR}/proxy.sh
 
-trap 'kill $RESTART_SELF_PID $READER_PID' EXIT SIGKILL
+trap 'kill $CHECK_UPDATE_PID $READER_PID' EXIT SIGKILL
 
-
-restart_self()
+update_self()
 {
-	trap 'kill $SLEEP_PID' EXIT SIGKILL
-	sleep $TIMEOUT_DAYS &
-	SLEEP_PID="$! "
+	git fetch origin main
+	[[ $? != 0 ]] && return -1
 
-	wait
-	kill $MAIN_SELF
+	local git_head_commit_old=$(git rev-parse HEAD)
+	local git_head_commit_new=$(git log remotes/origin/main -n1 --pretty=oneline | awk '{print $1}')
+	[[ "$git_head_commit_old" = "$git_head_commit_new" ]] && return 0
+
+	git checkout .
+	git reset --hard origin/main
+
+	return 1
 }
 
-# pushd $WORK_DIR
-# git checkout .
-# git reset --hard HEAD^
-# git config pull.rebase false
-# git pull
-# popd
+check_update()
+{
+	local check_interval=$1
+	local pid=$2
 
-restart_self &
-RESTART_SELF_PID="$! "
+	while true ; do
+		local ret=0
+		pushd $WORK_DIR &> /dev/null
+		update_self
+		ret=$?
+		popd &> /dev/null
+
+		if [[ $ret == 1 ]]; then
+			kill $pid
+		else
+			sleep $check_interval
+		fi
+	done
+}
+
+
+check_update $CHECK_UPDATE_DAYS $MAIN_SELF &
+CHECK_UPDATE_PID="$! "
+
+sleep 5
 
 if [[ -e $CLIENT_CONF ]]; then
 	bash ${WORK_DIR}/reader_frp.sh $(cat $CLIENT_CONF)  &
-	READER_PID=$!
+	FRP_PID=$!
 else
 	bash ${WORK_DIR}/reader_frp.sh -s -p "$@"  &
-	READER_PID=$!
+	FRP_PID=$!
 fi
 
-wait $READER_PID
+wait $FRP_PID
 
